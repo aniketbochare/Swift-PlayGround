e7eee9500f856ae078b68c348276761d77701434
aniketbochare <aniket6666@gmail.com>
2014-10-31 00:51:50 -0500

    My First Swift Commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,57 @@
+// Playground - noun: a place where people can play
+
+//import UIKit
+
+//var str = "Hello, playground"
+
+//var greeting = "Hello"
+//greeting += "World"
+//println(greeting)
+
+
+
+//var greeting = "Hello"
+//greeting.append(Character("!"))
+//println(greeting)
+//greeting = "hello world ".stringByAppendingString("of Swift")
+
+
+/*import Foundation
+var greeting = "hello world".capitalizedString
+println(greeting)
+
+var radius = 4
+let pi = 3.14159
+
+let million = 1_000_000
+
+var area = Double(radius) * Double(radius) * pi
+*/
+
+//var overflow = Int.max + 1
+
+//let alwaysTrue = true
+
+//var address: (Double,String) = (742, "Evergreen Terrace")
+
+//var address = (Double(742), "Evergreen Terrace")
+
+var address = (742.0, "Evergreen Terrace")
+
+let (house,street) = address
+
+println(house)
+println(street)
+
+//println(address.0)
+//println(address.1)
+
+
+//var myphone = ("iphone", 6)
+
+//println(myphone.0)
+//println(myphone.1)
+
+//myphone.1 = 7
+//myphone
+
